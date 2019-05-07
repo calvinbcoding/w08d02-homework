2f5a76bf8e07ec3c64d52ad40ae404d29a687e05
Calvin Bruch <calvinbcoding@gmail.com>
2019-05-06 22:22:44 -0600

    "file structure"
    Signed-off-by: Calvin Bruch <calvinbcoding@gmail.com>

--- calvin-hw/src/App.js
+++ calvin-hw/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import PokemonContainer from './PokemonContainer/PokemonContainer';
+import ItemsContainer from './ItemsContainer/ItemsContainer';
+import MovesContainer from './MovesContainer/MovesContainer';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      chosenPage: null
+    }
+  }
+  changePage = (e) => {
+    console.log(e.target.id);
+    this.setState({
+      chosenPage: e.target.id
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <button onClick={this.changePage} id="moves">Show me the moves!!!</button>
+        <button onClick={this.changePage} id="items">Show me the Items!!!</button>
+        <button onClick={this.changePage} id="pokemon">Show me the Pokemon!!!</button>
+        {
+          this.state.chosenPage === null ?
+            null
+            :
+            this.state.chosenPage === "moves" ?
+              <MovesContainer></MovesContainer>
+              :
+              this.state.chosenPage === "items" ?
+                <ItemsContainer></ItemsContainer>
+                :
+                <PokemonContainer></PokemonContainer>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
+
